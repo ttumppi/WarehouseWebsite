519b1bcbd1ae23c14b826d766d431efe91ee208f
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-07 20:26:31 +0300

    Debug.

--- backend/src/db/dbConnector.js
+++ backend/src/db/dbConnector.js
@@ -15,9 +15,9 @@ import * as fs from "node:fs"
 const ReadDBCreds = async () => {
 
     return new Promise( (resolve, reject) => {
-        fs.readFile("./creds.spec", "utf8", (error, data) => {
+        fs.readFile("creds.spec", "utf8", (error, data) => {
             if (error){
-                console.log("Failed to read credentials");
+                console.log(`Failed to read credentials ${error}`);
                 reject( new Error("No db credentials file"));
             }
             else{
