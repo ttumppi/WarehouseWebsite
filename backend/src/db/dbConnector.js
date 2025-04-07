2ad7cf22e2dccb4532e42f4ef1481f2fd1cb3db9
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-07 20:39:51 +0300

    Fixing credential file reading.

--- backend/src/db/dbConnector.js
+++ backend/src/db/dbConnector.js
@@ -1,9 +1,10 @@
 import Pool from "pg"
 import * as fs from "node:fs"
+import * as path from "path"
 
 
 
-
+const credentialsPath = path.join(__dirname, "creds.spec");
 
 
 
@@ -15,7 +16,7 @@ import * as fs from "node:fs"
 const ReadDBCreds = async () => {
 
     return new Promise( (resolve, reject) => {
-        fs.readFile("creds.spec", "utf8", (error, data) => {
+        fs.readFile(credentialsPath, "utf8", (error, data) => {
             if (error){
                 console.log(`Failed to read credentials ${error}`);
                 reject( new Error("No db credentials file"));
