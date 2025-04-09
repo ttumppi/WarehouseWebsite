0537419be44e2da9dadc9f6b02fe531d6cc9687f
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-09 21:19:58 +0300

    Removed console log from test.

--- backend/unitTests/parse_sql_commands_from_file.test.js
+++ backend/unitTests/parse_sql_commands_from_file.test.js
@@ -8,9 +8,6 @@ const parseFile = async () => {
 test("Prints each query seperately", async () => {
     const queries = await parseFile();
 
-    for (const query of queries){
-        console.log(query);
-    }
 
     expect(queries.length).toBe(4);
 });
