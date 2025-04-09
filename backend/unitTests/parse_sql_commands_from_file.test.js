b7239ca42db548eab58709dcd3cb9493a3087c77
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-09 20:48:41 +0300

    Added test to test file.

--- backend/unitTests/parse_sql_commands_from_file.test.js
+++ backend/unitTests/parse_sql_commands_from_file.test.js
@@ -2,4 +2,11 @@ import * as dbHandler from "../src/db/dbHandler.js"
 
 const parseFile = async () => {
     console.log(await dbHandler.GetSplitDBSetupQueries());
+
 }
+
+test("Prints each query seperately", async () => {
+    const queries = await parseFile();
+
+    expect(queries.length).toBe(4);
+});
