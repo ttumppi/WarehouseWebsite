6a1a8615e405d14abe8a0c85aedd7c1db10fc140
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-13 22:35:00 +0300

    D

--- backend/src/pageHandlers/loginHandler.js
+++ backend/src/pageHandlers/loginHandler.js
@@ -77,6 +77,7 @@ export const GetSalt = async (res, username) => {
         );
     }
 
+    console.log("Done");
     return res.status(200).json({success: true,
         salt: passwordResult.value.rows[0].salt
     });
