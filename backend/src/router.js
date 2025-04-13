5332b98c551e26108e8958d2bb9f156f7371f950
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-13 22:33:44 +0300

    D

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,11 +4,14 @@ import * as loginHandler from "./pageHandlers/loginHandler.js"
 
 export const RegisterRoutes = (server) => {
     server.post("/api/login", async (req, res) => {
-       return await loginHandler.CorrectLogin(req, res);
+
+        console.log("/login POST");
+        return await loginHandler.CorrectLogin(req, res);
     });
 
 
     server.get("/api/login/:username", async (req, res) => {
+        console.log("/login/:username GET");
         return await loginHandler.GetSalt(res, req.params.username);
     })
 }
