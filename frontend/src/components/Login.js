1fb3f62283d197b2be52b5abf1f60f990870ed4f
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-14 17:16:42 +0300

    D

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
           `http://ec2-54-204-100-237.compute-1.amazonaws.com:5000/api/shelfs`, {
           method: "GET",
           headers: { "Content-Type": "application/json" },
-          credentials: "same-origin"
+          credentials: "include"
         });
 
         console.log(await shelfsRes.json());
