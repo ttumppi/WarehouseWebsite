a76ff79207cb46cc5737d9a01db554b73f11ed6d
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-16 01:52:01 +0300

    D

--- frontend/src/components/ShelfItem.js
+++ frontend/src/components/ShelfItem.js
@@ -60,7 +60,9 @@ const ShelfItem = ({ loginNeeded }) => {
         initialBalance = shelfData.item.balance;
         initialLocation = shelfData.item.location;
         initialShelf = shelf;
-        console.log(initialBalance, initialLocation, initialShelf);
+        console.log(initialBalance === shelfData.item.balance &&
+             initialLocation === shelfData.item.location &&
+              initialShelf === shelf);
 
         const itemRes = await fetch(
             `http://ec2-54-204-100-237.compute-1.amazonaws.com:5000/api/item/${shelfData.item.item_id}`, {
