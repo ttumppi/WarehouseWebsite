5b3d8fda74479e9ddf5bb9e25436c266c6fd6204
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-16 21:22:52 +0300

    D

--- frontend/src/components/ChangePassword.js
+++ frontend/src/components/ChangePassword.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import CryptoJS from "crypto-js";
 import { GenerateRandomString } from "../stringFunctions";
 import { useParams } from "react-router-dom";
