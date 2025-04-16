af8f2844ec98ae15d2583dcacf40e4d7d81f7241
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-17 02:35:20 +0300

    D

--- frontend/src/components/ShelfsView.js
+++ frontend/src/components/ShelfsView.js
@@ -242,9 +242,9 @@ const ShelfsView = () => {
                     </button>}
                     
                     <ul>
-                        {(Object.keys(shelfItems).length > 0) && shelfItems[shelf.shelf_id].map((item) => {
+                        {(Object.keys(shelfItems).length > 0) && shelfItems[shelf.shelf_id].map((item) => (
                             <li key={item.id}> {item.manufacturer} - {item.model} - {item.serial}</li>
-                        })}
+                        ))}
                     </ul>
                     
                 </li>
