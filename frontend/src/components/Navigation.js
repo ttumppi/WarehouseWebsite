c6724e191823a9016bca0b89cf9d61fae0a7732f
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-16 14:53:08 +0300

    D

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navigation = ({ username, role }) => {
 
