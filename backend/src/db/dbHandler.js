870b9c90e0122a73d906a388c3ea555438921654
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-16 13:59:42 +0300

    D

--- backend/src/db/dbHandler.js
+++ backend/src/db/dbHandler.js
@@ -802,7 +802,7 @@ export const ChangeShelfSize = async (shelfName, newSize) => {
 
     const largestLocation = await GetLargestLocationInShelf(shelfName);
 
-    if (newSize <= largestLocation.value.rows[0].max_location){
+    if (newSize < largestLocation.value.rows[0].max_location){
         console.log("Size is too small for existing items");
         return {success: false,
             reason : "new size is too small for existing items"
