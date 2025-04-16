47fcda1319e2ce287f8cd8f89e00c258f20ee9a2
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-04-17 02:08:17 +0300

    D

--- frontend/src/components/ShelfsView.js
+++ frontend/src/components/ShelfsView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import StateContext from "./StateContext";
 import {Queue} from "../queue.js";
